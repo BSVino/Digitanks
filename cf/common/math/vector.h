4d8c81c13af1ecc5adbbaa64f005c20f47834fa6
Jorge Rodriguez <jorge@lunarworkshop.net>
2009-05-11 09:11:00 +0000

    Common files that should have been checked in with the other stuff.

--- cf/common/math/vector.h
+++ cf/common/math/vector.h
@@ -1,22 +1,105 @@
 #ifndef CF_VECTOR_H
 #define CF_VECTOR_H
 
+#include <math.h>
+
 typedef float unit_t;
 
 // Try to keep this class compatible with Valve's vector.h
 class Vector
 {
 public:
-	Vector(unit_t x, unit_t y, unit_t z);
+			Vector();
+			Vector(unit_t x, unit_t y, unit_t z);
+
+	Vector	operator-(void) const;
+
+	Vector	operator+(const Vector& v) const;
+	Vector	operator-(const Vector& v) const;
+	Vector	operator*(float s) const;
+	Vector	operator/(float s) const;
+
+	void	operator+=(const Vector &v);
+	void	operator-=(const Vector &v);
+	void	operator*=(float s);
+	void	operator/=(float s);
+
+	float	Length();
+	float	LengthSqr();
+	void	Normalize();
+	Vector	Normalized();
+
+	Vector	Cross(const Vector& v) const;
 
 	unit_t	x;
 	unit_t	y;
 	unit_t	z;
 };
 
+inline Vector::Vector()
+	: x(0), y(0), z(0)
+{
+}
+
 inline Vector::Vector(unit_t X, unit_t Y, unit_t Z)
 	: x(X), y(Y), z(Z)
 {
+}
+
+inline Vector Vector::operator-(const Vector& v) const
+{
+	return Vector(x-v.x, y-v.y, z-v.z);
+}
+
+inline Vector Vector::operator/(float s) const
+{
+	return Vector(x/s, y/s, z/s);
+}
+
+inline void Vector::operator+=(const Vector& v)
+{
+	x += v.x;
+	y += v.y;
+	z += v.z;
+}
+
+inline void Vector::operator-=(const Vector& v)
+{
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+}
+
+inline void Vector::operator/=(float s)
+{
+	x /= s;
+	y /= s;
+	z /= s;
+}
+
+inline float Vector::Length()
+{
+	return sqrt(x*x + y*y + z*z);
+}
+
+inline float Vector::LengthSqr()
+{
+	return x*x + y*y + z*z;
+}
+
+inline void Vector::Normalize()
+{
+	*this/=Length();
+}
+
+inline Vector Vector::Normalized()
+{
+	return *this/Length();
+}
+
+inline Vector Vector::Cross(const Vector& v) const
+{
+	return Vector(y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x);
 }
 
 #endif
--- cf/common/strutils.h
+++ cf/common/strutils.h
@@ -8,14 +8,14 @@
 #include <vector>
 
 // It's online inline so I don't have to make a strutils.cpp :P
-inline void strtok(const string& str, vector<string>& tokens, const string& delimiters = " \r\n\t")
+inline void strtok(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters = " \r\n\t")
 {
     // Skip delimiters at beginning.
-    string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+    std::string::size_type lastPos = str.find_first_not_of(delimiters, 0);
     // Find first "non-delimiter".
-    string::size_type pos     = str.find_first_of(delimiters, lastPos);
+    std::string::size_type pos     = str.find_first_of(delimiters, lastPos);
 
-    while (string::npos != pos || string::npos != lastPos)
+    while (std::string::npos != pos || std::string::npos != lastPos)
     {
         // Found a token, add it to the vector.
         tokens.push_back(str.substr(lastPos, pos - lastPos));
