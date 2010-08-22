068ec4be7e2cf29ae6e54ec39e0457c6787c45ec
Jorge Rodriguez <jorge@lunarworkshop.net>
2010-08-22 05:14:58 +0000

    Should really review code better before I commit.

--- digitanks/src/main.cpp
+++ digitanks/src/main.cpp
@@ -18,17 +18,16 @@ int main(int argc, char** argv)
 	// Make sure we open up an assert messagebox window instead of just aborting like it does for console apps.
 	_set_error_mode(_OUT_TO_MSGBOX);
 
+#ifndef _DEBUG
 	__try
 	{
 #endif
-
-		int* p = NULL;
-		*p = 42;
+#endif
 
 		// Put in a different function to avoid warnings and errors associated with object deconstructors and try/catch blocks.
 		CreateApplication(argc, argv);
 
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(_DEBUG)
 	}
 	__except (CreateMinidump(GetExceptionInformation()), EXCEPTION_EXECUTE_HANDLER)
 	{
