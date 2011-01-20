e0df9791e28c2ad8b47b2b8c945617f7084b73bd
Jorge Rodriguez <jorge@lunarworkshop.net>
2011-01-20 10:11:42 -0500

    Oops, wrong icon

--- digitanks/src/ui/hud.cpp
+++ digitanks/src/ui/hud.cpp
@@ -1098,7 +1098,7 @@ void CHUD::GetUnitSheet(unittype_t eUnit, int& sx, int& sy, int& sw, int& sh, in
 	else if (eUnit == STRUCTURE_CPU)
 	{
 		sx = 000;
-		sy = 100;
+		sy = 200;
 		sw = 100;
 		sh = 100;
 		return;
