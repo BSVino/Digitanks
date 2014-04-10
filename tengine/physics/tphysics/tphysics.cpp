d55855838b6f868795dcf2a437b7cf1a3188d148
Jorge Rodriguez <jorge@lunarworkshop.com>
2014-04-10 16:50:16 +0200

    Projectile motion for dynamic objects, based on the old digitanks code for the same. A bit more deterministic than the Bullet code for some reason.

--- tengine/physics/tphysics/tphysics.cpp
+++ tengine/physics/tphysics/tphysics.cpp
@@ -1,11 +1,207 @@
 #include "tphysics.h"
 
+#include <tinker/profiler.h>
+#include <tinker/cvar.h>
+#include <game/gameserver.h>
+
 CTPhysics::CTPhysics()
 {
+	// Allocate all memory up front to avoid reallocations
+	m_aEntityList.set_capacity(GameServer()->GetMaxEntities());
+
+	m_flSimulationTime = m_flGameTime = m_flServerTime = 0;
 }
 
 CTPhysics::~CTPhysics()
 {
+}
+
+void CTPhysics::AddEntity(IPhysicsEntity* pEntity, collision_type_t eCollisionType)
+{
+	TAssert(eCollisionType != CT_NONE);
+	if (eCollisionType == CT_NONE)
+		return;
+
+	TAssert(pEntity);
+	if (!pEntity)
+		return;
+
+	size_t iHandle = pEntity->GetHandle();
+	if (m_aEntityList.size() <= iHandle)
+		m_aEntityList.resize(iHandle + 1);
+
+	CPhysicsEntity* pPhysicsEntity = new (&m_aEntityList[iHandle]) CPhysicsEntity(pEntity, eCollisionType);
+}
+
+void CTPhysics::RemoveAllEntities()
+{
+	for (size_t i = 0; i < m_aEntityList.size(); i++)
+	{
+		auto pPhysicsEntity = &m_aEntityList[i];
+
+		RemoveEntity(pPhysicsEntity);
+	}
+}
+
+void CTPhysics::RemoveEntity(IPhysicsEntity* pEntity)
+{
+	CPhysicsEntity* pPhysicsEntity = GetPhysicsEntity(pEntity);
+	if (!pPhysicsEntity)
+		return;
+
+	RemoveEntity(pPhysicsEntity);
+}
+
+void CTPhysics::RemoveEntity(CPhysicsEntity* pPhysicsEntity)
+{
+	if (!pPhysicsEntity)
+		return;
+
+	pPhysicsEntity->m_pGameEntity = nullptr;
+}
+
+#ifdef _DEBUG
+#define PHYS_TIMESTEP "0.03333333333"
+#else
+#define PHYS_TIMESTEP "0.01666666666"
+#endif
+
+CVar phys_timestep("phys_timestep", PHYS_TIMESTEP);
+
+void CTPhysics::Simulate()
+{
+	TPROF("CTPhysics::Simulate");
+
+	if (!m_flServerTime)
+	{
+		m_flServerTime = GameServer()->GetGameTime();
+		return;
+	}
+
+	m_flGameTime += GameServer()->GetGameTime() - m_flServerTime;
+	m_flServerTime = GameServer()->GetGameTime();
+
+	m_apSimulateList.reserve(m_aEntityList.size());
+	m_apSimulateList.clear();
+
+	for (auto& oEntity : m_aEntityList)
+	{
+		if (!oEntity.m_pGameEntity)
+			continue;
+
+		if (!oEntity.m_bActive)
+			continue;
+
+		if (oEntity.m_eCollisionType != CT_DYNAMIC)
+			continue;
+
+		m_apSimulateList.push_back(&oEntity);
+	}
+
+	float flSimulationFrameTime = phys_timestep.GetFloat();
+
+	// Move all entities
+	for (auto& pEntity : m_apSimulateList)
+	{
+		// Break simulations up into very small steps in order to preserve accuracy.
+		// I think floating point precision causes this problem but I'm not sure. Anyway this works better for my projectiles.
+		for (double flCurrentSimulationTime = m_flSimulationTime; flCurrentSimulationTime < m_flGameTime; flCurrentSimulationTime += flSimulationFrameTime)
+		{
+			Vector vecVelocity = pEntity->m_vecVelocity;
+			Matrix4x4 mPhysics = pEntity->m_pGameEntity->GetPhysicsTransform();
+
+			Vector vecOrigin = mPhysics.GetTranslation() + vecVelocity * flSimulationFrameTime;
+			mPhysics.SetTranslation(vecOrigin);
+			pEntity->m_pGameEntity->SetPhysicsTransform(mPhysics);
+
+			pEntity->m_vecVelocity = vecVelocity + pEntity->m_vecGravity * flSimulationFrameTime;
+		}
+	}
+
+	while (m_flSimulationTime < m_flGameTime)
+		m_flSimulationTime += flSimulationFrameTime;
+
+	TStubbed("Trigger collision");
+#if 0
+	for (auto& pEntity : m_apSimulateList)
+	{
+		if (!pEntity->m_pGameEntity)
+			continue;
+
+		for (auto& pOther : m_apSimulateList)
+		{
+			if (!pOther->m_pGameEntity)
+				continue;
+
+			if (!pEntity->m_pGameEntity->ShouldCollideWith(pOther->m_pGameEntity->GetHandle(), pOther->m_pGameEntity->GetPhysicsTransform().GetTranslation()))
+				continue;
+
+			Vector vecPoint;
+			if (pEntity->m_pGameEntity->IsTouching(pEntity2, vecPoint))
+			{
+				pEntity->SetOrigin(vecPoint);
+				pEntity->Touching(pEntity2);
+			}
+		}
+	}
+#endif
+}
+
+void CTPhysics::SetEntityCollisionDisabled(IPhysicsEntity* pEnt, bool bDisabled)
+{
+	CPhysicsEntity* pEntity = GetPhysicsEntity(pEnt);
+
+	if (pEntity)
+		pEntity->m_bActive = !bDisabled;
+}
+
+void CTPhysics::SetEntityVelocity(IPhysicsEntity* pEnt, const Vector& vecVelocity)
+{
+	CPhysicsEntity* pEntity = GetPhysicsEntity(pEnt);
+
+	if (pEntity)
+		pEntity->m_vecVelocity = vecVelocity;
+}
+
+void CTPhysics::SetEntityGravity(IPhysicsEntity* pEnt, const Vector& vecGravity)
+{
+	CPhysicsEntity* pEntity = GetPhysicsEntity(pEnt);
+
+	if (pEntity)
+		pEntity->m_vecGravity = vecGravity;
+}
+
+void CTPhysics::SetEntityTransform(IPhysicsEntity* pEnt, const Matrix4x4& mTransform)
+{
+	// No op, we always use pEnt->GetPhysicsTransform() as the current transform.
+}
+
+Vector CTPhysics::GetEntityVelocity(IPhysicsEntity* pEnt)
+{
+	CPhysicsEntity* pEntity = GetPhysicsEntity(pEnt);
+
+	if (pEntity)
+		return pEntity->m_vecVelocity;
+
+	TAssert(false);
+	return Vector();
+}
+
+CPhysicsEntity* CTPhysics::GetPhysicsEntity(IPhysicsEntity* pEnt)
+{
+	TAssert(pEnt);
+	if (!pEnt)
+		return NULL;
+
+	size_t iHandle = pEnt->GetHandle();
+	TAssert(m_aEntityList.size() > iHandle);
+	if (m_aEntityList.size() <= iHandle)
+		return NULL;
+
+	CPhysicsEntity* pPhysicsEntity = &m_aEntityList[iHandle];
+	TAssert(pPhysicsEntity);
+
+	return pPhysicsEntity;
 }
 
 CPhysicsModel* CreatePhysicsModel()
--- tengine/physics/tphysics/tphysics.h
+++ tengine/physics/tphysics/tphysics.h
@@ -2,6 +2,36 @@
 
 #include "../physics.h"
 
+class CPhysicsEntity
+{
+public:
+	CPhysicsEntity()
+	{
+		m_pGameEntity = nullptr;
+		m_bActive = false;
+	};
+
+	CPhysicsEntity(IPhysicsEntity* pEntity, collision_type_t eCollisionType)
+	{
+		m_pGameEntity = pEntity;
+		m_eCollisionType = eCollisionType;
+		m_bActive = true;
+	};
+
+	~CPhysicsEntity()
+	{
+	};
+
+public:
+	IPhysicsEntity*  m_pGameEntity;
+	collision_type_t m_eCollisionType;
+
+	bool m_bActive;
+
+	Vector m_vecVelocity;
+	Vector m_vecGravity;
+};
+
 class CTPhysics : public CPhysicsModel
 {
 public:
@@ -9,12 +39,13 @@ public:
 	~CTPhysics();
 
 public:
-	virtual void    AddEntity(IPhysicsEntity* pEnt, collision_type_t eCollisionType) { TUnimplemented(); };
-	virtual void    RemoveEntity(IPhysicsEntity* pEnt) { TUnimplemented(); };
+	virtual void    AddEntity(IPhysicsEntity* pEnt, collision_type_t eCollisionType);
+	virtual void    RemoveEntity(IPhysicsEntity* pEnt);
+	virtual void    RemoveEntity(CPhysicsEntity* pEntity);
 	virtual size_t  AddExtra(size_t iExtraMesh, const Vector& vecOrigin) { TUnimplemented(); return ~0; };  // Input is result from LoadExtraCollisionMesh
 	virtual size_t  AddExtraBox(const Vector& vecCenter, const Vector& vecSize) { TUnimplemented(); return ~0; };
 	virtual void    RemoveExtra(size_t iExtra) { TUnimplemented(); };               // Input is result from AddExtra*
-	virtual void    RemoveAllEntities() { TUnimplemented(); };
+	virtual void    RemoveAllEntities();
 	virtual bool    IsEntityAdded(IPhysicsEntity* pEnt) { TUnimplemented(); return false; };
 
 	virtual void    LoadCollisionMesh(const tstring& sModel, bool bConcave, size_t iTris, const int* aiTris, size_t iVerts, const float* aflVerts) { TUnimplemented(); };
@@ -22,21 +53,21 @@ public:
 	virtual size_t  LoadExtraCollisionMesh(size_t iTris, bool bConcave, int* aiTris, size_t iVerts, float* aflVerts) { TUnimplemented(); return ~0; };
 	virtual void    UnloadExtraCollisionMesh(size_t iMesh) { TUnimplemented(); };
 
-	virtual void Simulate() { TUnimplemented(); };
+	virtual void Simulate();
 
 	virtual void DebugDraw(int iLevel) { TUnimplemented(); };
 
 	virtual collision_type_t GetEntityCollisionType(IPhysicsEntity* pEnt) { TUnimplemented(); return CT_NONE; };
 
 	virtual bool    IsEntityCollisionDisabled(IPhysicsEntity* pEnt) { TUnimplemented(); return true; };
-	virtual void    SetEntityCollisionDisabled(IPhysicsEntity* pEnt, bool bDisabled) { TUnimplemented(); };
-	virtual void    SetEntityTransform(IPhysicsEntity* pEnt, const Matrix4x4& mTransform) { TUnimplemented(); };
-	virtual void    SetEntityVelocity(IPhysicsEntity* pEnt, const Vector& vecVelocity) { TUnimplemented(); };
-	virtual Vector  GetEntityVelocity(IPhysicsEntity* pEnt) { TUnimplemented(); return Vector(0, 0, 0); };
+	virtual void    SetEntityCollisionDisabled(IPhysicsEntity* pEnt, bool bDisabled);
+	virtual void    SetEntityTransform(IPhysicsEntity* pEnt, const Matrix4x4& mTransform);
+	virtual void    SetEntityVelocity(IPhysicsEntity* pEnt, const Vector& vecVelocity);
+	virtual Vector  GetEntityVelocity(IPhysicsEntity* pEnt);
 	virtual void    SetControllerMoveVelocity(IPhysicsEntity* pEnt, const Vector& vecVelocity) { TUnimplemented(); };
 	virtual const Vector GetControllerMoveVelocity(IPhysicsEntity* pEnt) { TUnimplemented(); return Vector(0, 0, 0); }
 	virtual void    SetControllerColliding(IPhysicsEntity* pEnt, bool bColliding) { TUnimplemented(); };
-	virtual void    SetEntityGravity(IPhysicsEntity* pEnt, const Vector& vecGravity) { TUnimplemented(); };
+	virtual void    SetEntityGravity(IPhysicsEntity* pEnt, const Vector& vecGravity);
 	virtual void    SetEntityUpVector(IPhysicsEntity* pEnt, const Vector& vecUp) { TUnimplemented(); };
 	virtual void    SetLinearFactor(IPhysicsEntity* pEnt, const Vector& vecFactor) { TUnimplemented(); };
 	virtual void    SetAngularFactor(IPhysicsEntity* pEnt, const Vector& vecFactor) { TUnimplemented(); };
@@ -48,4 +79,14 @@ public:
 	virtual void CheckSphere(CTraceResult& tr, float flRadius, const Vector& vecCenter, IPhysicsEntity* pIgnore = nullptr) { TUnimplemented(); };
 
 	virtual void CharacterJump(IPhysicsEntity* pEnt) { TUnimplemented(); };
+
+	CPhysicsEntity* GetPhysicsEntity(IPhysicsEntity* pEnt);
+
+private:
+	tvector<CPhysicsEntity>  m_aEntityList;
+	tvector<CPhysicsEntity*> m_apSimulateList;
+
+	double m_flSimulationTime;
+	double m_flGameTime;
+	double m_flServerTime;
 };
