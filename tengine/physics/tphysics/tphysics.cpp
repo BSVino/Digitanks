e161fa92a0e300185127a27aba043810345576dd
Jorge Rodriguez <jorge@lunarworkshop.com>
2014-04-12 22:02:59 +0200

    Fix warning.

--- tengine/physics/tphysics/tphysics.cpp
+++ tengine/physics/tphysics/tphysics.cpp
@@ -225,7 +225,7 @@ void CTPhysics::Simulate()
 			CTraceResult tr;
 			DetectCollisions(pEntity, mOldPhysics, mNewPhysics, tr);
 
-			for (int i = 0; i < tr.m_aHits.size(); i++)
+			for (size_t i = 0; i < tr.m_aHits.size(); i++)
 			{
 				auto& oOther = tr.m_aHits[i];
 
